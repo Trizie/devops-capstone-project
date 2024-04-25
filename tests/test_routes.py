1a17e4ec3b94a306b3b94d3bc3e389ff46a2b4ad
Trizie <patrizia.stutzenstein@gmx.at>
2024-04-25 06:01:22 -0400

    update on update function TDD

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -143,10 +143,15 @@ class TestAccountService(TestCase):
     def test_update_account(self):
         """It should update an existing account"""
         #create an account
-        test_account = self._create_accounts(1)[0]
-        self.assertEqual(test_account.status_code, status.HTTP_201_CREATED)
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
 
         #update the account
-        new
-    
+        new_account = resp.get_json()
+        new_account["name"] = "Peter Fresh"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Peter Fresh")
 
