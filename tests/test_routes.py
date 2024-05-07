6f5ebf669ddc47ddc2a9a27895016d11893b3aaa
Trizie <patrizia.stutzenstein@gmx.at>
2024-05-07 05:59:46 -0400

    lint update

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,7 +139,7 @@ class TestAccountService(TestCase):
         """
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-    
+
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -154,13 +154,13 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
-    
+
     def test_delete_account(self):
         """It should delete an existing account"""
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
-    
+
     def test_get_account_list(self):
         """It should get a list of accounts"""
         self._create_accounts(5)
@@ -173,7 +173,3 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-
-
-                
